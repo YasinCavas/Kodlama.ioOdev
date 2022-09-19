0a5920953ef9474249cea4c899bb32840c6641a5
YasinCavas <yasincavas16@gmail.com>
2022-09-19 23:52:30 +0300

    Lesson11

---
+++ Lesson11/src/loopDemo.java
@@ -0,0 +1,17 @@
+
+public class loopDemo {
+
+	public static void main(String[] args) {
+	
+		// For i++ ile i=i+1 aynı olaydır
+		// int i=1 sayaç tanıma kısmı
+		// i<10 şart kısmı
+		for (int i = 2; i < 10; i += 2) {
+			System.out.println(i);
+
+		}
+		System.out.println("Döngü Bitti");
+
+	}
+
+}
