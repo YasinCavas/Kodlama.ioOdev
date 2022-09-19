f8ef199b4c15f2caa8cda46fec3536c008de6180
YasinCavas <yasincavas16@gmail.com>
2022-09-19 23:51:18 +0300

    lesson7

---
+++ Lesson7/Lesson6/src/Variables.java
@@ -0,0 +1,17 @@
+
+public class Variables {
+
+	// camel
+	public static void main(String[] args) {
+		
+		int ogrencisayisi = 12;
+		String mesaj = "Öğrenci sayısı :  ";
+		System.out.println(mesaj + ogrencisayisi);
+		System.out.println(mesaj + ogrencisayisi);
+		System.out.println("Öğrenci Sayım : " + ogrencisayisi);
+		System.out.println("Öğrenci Sayım: " + ogrencisayisi);
+		System.out.println("Öğrenci Sayım: " + ogrencisayisi);
+
+	}
+
+}
