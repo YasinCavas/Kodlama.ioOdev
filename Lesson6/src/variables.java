eeceeed65e49442c7eb42f6ebc9e58d70f98b2c6
YasinCavas <yasincavas16@gmail.com>
2022-09-20 16:56:31 +0300

    Lesson6

---
+++ Lesson6/src/variables.java
@@ -0,0 +1,15 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+
+		int ogrencisayi = 13;
+
+		String mesaj = "Öğrenci Sayısı:";
+
+		System.out.println(mesaj + ogrencisayi);
+		System.out.println(mesaj + ogrencisayi);
+		System.out.println(mesaj + ogrencisayi);
+		System.out.println(mesaj + ogrencisayi);
+	}
+}
